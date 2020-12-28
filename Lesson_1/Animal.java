4dba468b4ae07bc9509f9903815ea25a9bfa22dc
andrey <sveshennik@ya.ru>
2020-12-28 09:59:41 +0300

    Home work

---
+++ Lesson_1/Animal.java
@@ -0,0 +1,16 @@
+package Lesson_1;
+
+public abstract class Animal {
+
+    protected String name;
+
+    public Animal(String name) {
+        this.name = name;
+    }
+
+//    abstract void calc();
+
+    void info() {
+        System.out.println("Animal " + name);
+    }
+}
